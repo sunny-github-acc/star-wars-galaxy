396eac7a72c091f87de9b024412f4c265cbdea3e
sunny-github-acc <karolis.kazak@gmail.com>
2025-09-24 11:56:41 +0300

    feat: update layout component for responsive design and improved styling

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -2,12 +2,17 @@ import Link from 'next/link';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='flex h-screen'>
-      <main className='flex-1 p-8 overflow-y-auto'>
+    <div className='flex flex-col md:flex-row h-screen'>
+      <main className='flex-1 p-4 md:p-8 overflow-y-auto'>
         {children}
       </main>
 
-      <aside className='w-64 bg-black/30 p-4 border-r border-imperial-blue-glow/30'>
+      <aside
+        className={
+          'w-full md:w-64 bg-black/30 p-4 border-b ' +
+          'md:border-b-0 md:border-r border-imperial-blue-glow/30'
+        }
+      >
         <Link href='/'>
           <h1 className='text-xl font-bold text-white mb-8'>
             ISB HOLONET
